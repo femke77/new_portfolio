8feb16c10147c1d316aefeb4d57cd375c4a1a224
femke77 <megan.meyers.388@gmail.com>
2025-01-06 21:51:27 -0800

    readme and project added

--- src/assets/projectImages/index.js
+++ src/assets/projectImages/index.js
@@ -5,4 +5,5 @@ export { default as cuccio } from './cuccio.png';
 export { default as readme } from './readme.png';
 export { default as adam } from './adam.png';
 export { default as blog } from './blog.png';
+export { default as portfolio } from './portfolio.png';
 
