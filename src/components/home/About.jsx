20379d91f758ff41cf58a103d7db2a9d9194238c
femke77 <megan.meyers.388@gmail.com>
2025-02-05 19:15:40 -0800

    style fixes

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,6 +1,6 @@
 import Grid from '@mui/system/Unstable_Grid';
 import styled from '@mui/system/styled';
-import useScript from '../../hooks/useScript';
+import useScript from '../../hooks/UseScript';
 
 const Item = styled('div')(({ theme }) => ({
   background: 'transparent',
